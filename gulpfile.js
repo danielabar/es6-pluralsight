6ef0baebd54b709b7804070d1638bd73d439bcb3
Daniela Baron <baron.daniela@gmail.com>
2015-10-12 07:30:29 -0400

    use gulp to run the tests

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var karma = require('karma').server;
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js'
+  }, function() {
+    done();
+  });
+});
